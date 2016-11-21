0347c80643d463664b89ebdcd251a7cd92b63810
Nate Rudnick Aldridge <nr265@cornell.edu>
2016-11-21 02:29:09 +0000

    Added sort by stars feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,22 @@ class HomeController < ApplicationController
   def search
     base_url = "https://api.github.com/search/repositories?q="
     
-    @page = nil
+    search_term = params[:q]
+    
+    language_query = "+language:"
+    language_term = params[:language]
+    if language_term == ""
+      language_query = ""
+    else
+      language_query += language_term
+    end
+    
+    sort = ""
+    if params[:sort] == "true"
+      sort = "&sort=stars&order=desc"
+    end
+    
+    
     @page_number = params[:page]
     if @page_number == nil
       page_term = ""
@@ -15,18 +30,11 @@ class HomeController < ApplicationController
     
     pagination_term = "&per_page=25"
     
-    language_query = "+language:"
     
-    search_term = params[:q]
     
-    language_term = params[:language]
-    if language_term == ""
-      language_query = ""
-    else
-      language_query += language_term
-    end
     
-    final_url = base_url + search_term + language_query + pagination_term + page_term
+    
+    final_url = base_url + search_term + language_query + pagination_term + page_term + sort
     
     response = RestClient.get(final_url)
     @repos = JSON.parse(response.body)
